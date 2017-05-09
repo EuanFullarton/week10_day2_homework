1d5e671aab6623510e18cbcfce69f9c44daf8b21
Euan Fullarton <euanfullarton7@gmail.com>
2017-05-09 22:13:17 +0100

    eventually managed to get 'remove all of type' function working

--- park.js
+++ park.js
@@ -2,4 +2,22 @@ var Park = function(){
   this.enclosure = [];
 };
 
+Park.prototype = {
+  addDinosaur: function(dinosaur){
+    this.enclosure.push(dinosaur);
+  },
+
+  removeDinosaurOfType: function(dinosaur){
+
+    for( i=this.enclosure.length-1; i>=0; i--) {
+        if( this.enclosure[i].type == dinosaur.type) this.enclosure.splice(i,1);
+    }
+  },
+
+  empty: function(){
+    this.enclosure = [];
+  }
+};
+
+
 module.exports = Park;
--- specs/park_test.js
+++ specs/park_test.js
@@ -6,10 +6,53 @@ describe("Park", function(){
 
   beforeEach(function(){
     this.park = new Park();
+    this.dinosaur1 = new Dinosaur("T-Rex", 2);
+    this.dinosaur2 = new Dinosaur("Triceratops", 3);
+    this.dinosaur3 = new Dinosaur("Pterodactyl", 6);
   });
 
   it("enclosure should start empty", function(){
     assert.equal(0, this.park.enclosure);
+  });
+
+  it("should be able to add dinosaur to enclosure", function(){
+    this.park.addDinosaur(this.dinosaur1);
+    assert.equal(1, this.park.enclosure.length);
+  });
+
+  // it("should return all animals in enclosure", function(){
+  //   this.park.addDinosaur(this.dinosaur1);
+  //   this.park.addDinosaur(this.dinosaur1);
+  //   this.park.addDinosaur(this.dinosaur2);
+  //   assert.equal(0, this.park.removeDinosaurOfType(this.dinosaur1));
+  // })
+
+  it("should be able to empty enclosure", function(){
+    this.park.addDinosaur(this.dinosaur1);
+    this.park.addDinosaur(this.dinosaur2);
+    this.park.empty();
+    assert.equal(0, this.park.enclosure.length);
+  });
+
+  it("should be able to remove all dinosaurs of specified type", function(){
+    //testing with T-Rex
+    this.park.addDinosaur(this.dinosaur1);
+    this.park.addDinosaur(this.dinosaur1);
+    this.park.addDinosaur(this.dinosaur2);
+    assert.equal(3, this.park.enclosure.length);
+    this.park.removeDinosaurOfType(this.dinosaur1);
+    assert.equal(1, this.park.enclosure.length);
+
+    this.park.empty();
+
+    //testing with Triceratops
+    this.park.addDinosaur(this.dinosaur1);
+    this.park.addDinosaur(this.dinosaur1);
+    this.park.addDinosaur(this.dinosaur2);
+    assert.equal(3, this.park.enclosure.length);
+    this.park.removeDinosaurOfType(this.dinosaur2);
+    assert.equal(2, this.park.enclosure.length);
+
   });
 
 });
